060e7d059231f8b0a667527f3af81a2816cd92ed
FranciscoMaik <chiconunes3081@gmail.com>
2022-02-10 16:42:48 -0300

    feat: adding legend

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import { generateLibTree } from '@nivo/generators'
 import { colorSchemes } from '@nivo/colors'
-import { ResponsiveSunburst, ComputedDatum, DatumId } from '@nivo/sunburst';
+import { ResponsiveSunburst, DatumId } from '@nivo/sunburst';
 
 
 import { data } from '../../utils/constants/data';
@@ -12,27 +11,60 @@ const Container = styled.div`
     flex-direction: column;
     width: 100%;
     height: 600px;
+
+    .button {
+        background-color: blue;
+        color: white;
+        max-width: 200px;
+        margin-left: 200px;
+        border-radius: 4px;
+        border: none;
+        padding: 8px 16px;
+        cursor: pointer;
+
+        :hover {
+            background-color: #2157e0;
+        }
+    }
 `;
-interface RawDatum {
-    name: string
-    loc?: number | undefined
-}
 
+const Legends = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: -100px;
+    margin-left: 200px;
 
-console.log("Object generate three", generateLibTree());
+    > div{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 12px;
+        max-height: 30px;
+        
+    }
+    .color {
+        height: 20px;
+        width: 20px;
+        padding: 0px;
+    }
+    
+    
+`;
+// interface RawDatum {
+//     name: string
+//     loc?: number | undefined
+// } -> Interface para a personalização de cores
 
 export const Chart: React.FC = () => {
     const [dataItem, setDataItem] = useState(data);
-    const drillDownColors = colorSchemes.brown_blueGreen[7]
-    const drillDownColorMap = {
-        Caixa: drillDownColors[0],
-        "Renda Fixa": drillDownColors[1],
-        "Renda Variável": drillDownColors[2],
-        "Fundos Imobiliários": drillDownColors[3],
-        "Fundos de Investimento": drillDownColors[4],
-        text: drillDownColors[5],
-        misc: drillDownColors[6],
-    }
+    const drillDownColors = colorSchemes.set2
+    // const drillDownColorMap = {
+    //     Caixa: drillDownColors[0],
+    //     "Renda Fixa": drillDownColors[1],
+    //     "Renda Variável": drillDownColors[2],
+    //     "Fundos Imobiliários": drillDownColors[3],
+    //     "Fundos de Investimento": drillDownColors[4],
+    // } -> para a função de personalização de cores
 
     const findObject = (dataOther: any[], name: DatumId) => dataOther.find((searchedName: { name: string; }) => searchedName.name === name)
 
@@ -45,42 +77,48 @@ export const Chart: React.FC = () => {
         return [...acc, item]
     }, [])
 
-    const getDrillDownColor = (node: Omit<ComputedDatum<RawDatum>, 'color' | 'fill'>) => {
-        const category = [...node.path].reverse()[1] as keyof typeof drillDownColorMap
+    // const getDrillDownColor = (node: Omit<ComputedDatum<RawDatum>, 'color' | 'fill'>) => {
+    //     const category = [...node.path].reverse()[1] as keyof typeof drillDownColorMap
     
-        return drillDownColorMap[category]
+    //     return drillDownColorMap[category]
+    // } -> Função para personalizar as cores do filho como o do pai
+    
+    const calcPercent = (data: {name: string, color: string, loc: number}[], value: number) => {
+        const total = data.reduce((acc,item) => acc + item.loc, 0);
+        return Number(((100 * value) / total).toFixed(0));
     }
 
     const handleClick = (clickedData: { id: DatumId; }, event: any) => {
         const foundObject = findObject(flatten(data.children), clickedData.id);
-        if (foundObject && foundObject.children) {
-          setDataItem(foundObject);
+        if (foundObject && foundObject.children) {         
+          setDataItem({...foundObject, children: foundObject.children.map((item: any) => ({...item, loc: calcPercent(foundObject.children, item.loc)}))});
         }
       };
 
-    console.log(findObject);
-
     return (
         <Container>
-            <button type='button' onClick={() => setDataItem(data)}>Redefinir</button>
+            <button type='button' className='button' onClick={() => setDataItem(data)}>Redefinir</button>
             <ResponsiveSunburst
+                valueFormat=">-.2"
+                value="loc"
+                id="name"
                 isInteractive
                 data={dataItem}
-                margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
-                id="name"
-                value="loc"
-                colors={getDrillDownColor}
+                cornerRadius={3}
+                margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+                // colors={getDrillDownColor}
+                colors={{scheme: "set2"}}
                 borderWidth={1}
-                inheritColorFromParent={false}
+                inheritColorFromParent
                 borderColor={{
                     from: 'color',
                     modifiers: [['darker', 0.6]],
                 }}
                 animate={true}
                 enableArcLabels
-                arcLabel="formattedValue"
+                arcLabel={d => `${d.value}%`}
                 arcLabelsRadiusOffset={0.45}
-                arcLabelsSkipAngle={12}
+                arcLabelsSkipAngle={8}
                 arcLabelsTextColor={{
                     from: 'color',
                     modifiers: [
@@ -90,7 +128,7 @@ export const Chart: React.FC = () => {
                         ]
                     ]
                 }}
-                transitionMode="startAngle"
+                transitionMode="middleAngle"
                 onClick={handleClick}
                 theme={{
                     tooltip: {
@@ -98,9 +136,24 @@ export const Chart: React.FC = () => {
                             background: '#333',
                         },
                     },
+                    labels: {
+                        text: {
+                            fontSize: 18,
+                            fontWeight: 'bold'
+                        }
+                    }
                 }}
-
             />
+
+            <Legends>
+                {dataItem.children.map((item,index) => (
+                    <div>
+                        <div className="color" style={{ background: `${drillDownColors[index]}` }} />
+                        <p>{item.name}</p>
+                    </div>
+                ))}
+                
+            </Legends>
         </Container>
     );
 }
